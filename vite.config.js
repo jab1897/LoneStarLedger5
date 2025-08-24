09ad3a7f99c608348fb432f1f133483447d43c2a
jab1897 <138234117+jab1897@users.noreply.github.com>
2025-08-23 19:07:15 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,13 @@
 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    strictPort: false
+  },
+  build: { outDir: "dist" },
+  base: "/"
+});
